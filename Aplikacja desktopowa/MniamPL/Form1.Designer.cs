312b81288510744c06bbdbec39d8bd257ec03bb8
Bartosz Kaczorowski <61693294+BartoszKaczorowski@users.noreply.github.com>
2020-03-22 15:56:43 +0100

    Add files via upload

---
+++ Aplikacja desktopowa/MniamPL/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace MniamPL {
+    partial class Form1 {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasło";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(88, 127);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(89, 34);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Zaloguj";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(61, 58);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(219, 22);
+            this.loginTextBox.TabIndex = 3;
+            // 
+            // passTextBox
+            // 
+            this.passTextBox.Location = new System.Drawing.Point(61, 89);
+            this.passTextBox.Name = "passTextBox";
+            this.passTextBox.Size = new System.Drawing.Size(219, 22);
+            this.passTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 206);
+            this.Controls.Add(this.passTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logowanie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passTextBox;
+    }
+}
+
---
+++ Aplikacja desktopowa/MniamPL/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MniamPL {
+    public partial class Form1 : Form {
+        PanelGlowny main;
+        MySqlConnection connection = new MySqlConnection("Server=heltica.cba.pl;Port=3306;Database=heltica;Uid=helticadb;Pwd=Helticadb1;");
+        MySqlCommand command;
+        int i;
+        public Form1() {
+            InitializeComponent();
+            passTextBox.PasswordChar = '•';
+        }
+
+        private void loginButton_Click(object sender, EventArgs e) {
+            openConnection();
+            string com = "SELECT OB.id_lokalu FROM m_uzytkownicy U, m_obslugalokalu OB WHERE U.login='" + loginTextBox.Text + "' AND U.haslo='" + passTextBox.Text + "' AND U.id_uzytkownika=OB.id_uzytkownika;";
+            command = new MySqlCommand(com, connection);
+            command.ExecuteNonQuery();
+            MySqlDataAdapter ad = new MySqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            i = Convert.ToInt32(dt.Rows.Count.ToString());
+            if (i == 1) {
+                main = new PanelGlowny(this, loginTextBox.Text, int.Parse(dt.Rows[0][0].ToString()));
+                main.Show();
+                this.Hide();
+            } else {
+                MessageBox.Show("Błędne dane logowania");
+            }
+            closeConnection();
+            loginTextBox.Clear();
+            passTextBox.Clear();
+        }
+
+        public void openConnection() {
+            if (connection.State == ConnectionState.Closed) {
+                connection.Open();
+            }
+        }
+
+        public void closeConnection() {
+            if (connection.State == ConnectionState.Open) {
+                connection.Close();
+            }
+        }
+    }
+}
