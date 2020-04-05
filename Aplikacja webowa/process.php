818e5be243548082d8862a6ba7a80f455908013c
Michał Górski <61849577+m-gorski@users.noreply.github.com>
2020-04-05 17:18:03 +0200

    Update process.php

--- Aplikacja webowa/process.php
+++ Aplikacja webowa/process.php
@@ -1,48 +1,35 @@
-<!-- dodanie danych do bazy -->
-
-<?php
+<!-- pobranie danych wejściowych -->
+<?php 
 require_once('connection.php');
 
-
+session_start();
+    if(isset($_POST['Login']))
+    {
+        //jeżeli inputy od loginu i hasla puste to komunikat
+       if(empty($_POST['Username']) || empty($_POST['Password']))
+       {
+            header("location:index.php?Empty= Please Fill in the Blanks");
+       }
+       else
+       {
+            $query="select * from m_uzytkownicy where login='".$_POST['Username']."' and haslo='".$_POST['Password']."'";
+            $result=mysqli_query($connection,$query);
  
-
-$userName = $_POST['username'];
-// $email = $_POST['email'];
-$password = $_POST['password'];
-$password2 = $_POST['confirm_password'];
-
-
- //  SPRAWDZAM CZY UŻYTKOWNIK O PODANEJ NAZWIE JUŻ ISTNEIEJE
-$sql_u = "SELECT * FROM m_uzytkownicy WHERE login='".$_POST['username']."'";
-$res_u = mysqli_query($connection, $sql_u);
-
-//JEŻELI UŻYTKOWNIK O PODANEJ NAZWIE ISTNIEJE WYŚWIETLAM WIADOMOŚĆ
-if (mysqli_num_rows($res_u) > 0) {
-        ?><script>window.location.href = "index.php?Username=false";</script><?php
-        
- }
-
- //  JEŻELI OBA WPROWADZONE HASŁA SĄ TAKIE SAME TO WRZUCAMY DO BAZY, JAK NIE WYŚWIETLAM WIADOMOŚĆ
- elseif($password == $password2){
-
-    $sql = "INSERT INTO m_uzytkownicy (login, haslo) VALUES ('$userName','$password')";
-    $result = mysqli_query($connection, $sql);
-
-     if($result)
-         {
-             ?><script>window.location.href = "index.php?registered=true";</script><?php
-         }
-
-         else {
-        // failed :(
-         echo "Error :".$sql;
-        }
-
- }
- //JEŻELI HASŁA SIĘ NIE ZGADZAJĄ WYŚWIETLAM WIADOMOŚĆ
-else
-{
-    ?><script>window.location.href = "index.php?registered=false";</script><?php
-}
-
+            if(mysqli_fetch_assoc($result))
+            {
+                $_SESSION['User']=$_POST['Username'];
+                //header("location:welcome.php");
+                 header("location:index.php?Logged=true");
+            }
+            else
+            {
+                header("location:index.php?Invalid= Please Enter Correct User Name and Password ");
+            }
+       }
+    }
+    else
+    {
+        echo 'Not Working Now Guys';
+    }
+ 
 ?>
