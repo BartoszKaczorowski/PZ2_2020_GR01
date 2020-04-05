004fe0b64e9608722782fea54b7665e6026aec32
Michał Górski <61849577+m-gorski@users.noreply.github.com>
2020-04-05 17:13:04 +0200

    Obsługa formularzy i podpięcie biblioteki

--- Aplikacja webowa/file.js
+++ Aplikacja webowa/file.js
@@ -1,9 +1,33 @@
 // pobranie elementu modalnego
 var modal = document.getElementById('modalElement');
+var modal2 = document.getElementById('modalElement2');
 
-// jeśli użytkownik kliknie gdziekolwiek to okno modalne zostanie zamknięte
-window.onclick = function (event) {
-    if (event.target == modal) {
+window.onload = function () {
+
+    document.getElementById("modalElement").style.display = "none";
+    document.getElementById("modalElement2").style.display = "none";
+
+};
+
+
+function myFunction() {
+    if (modal.style.display == "none") {
+        modal.style.display = "block";
+    } else {
         modal.style.display = "none";
     }
 }
+
+function myFunction2() {
+    if (modal2.style.display === "none") {
+        modal2.style.display = "block";
+        modal.style.display = "none";
+    } else {
+        modal2.style.display = "none";
+    }
+}
+
+//biblioteka do animacji przy scrollowaniu
+AOS.init({
+    duration: 1200,
+});
