61f566ad136c14d3427e377b2a1cbc4de173532c
Michał Górski <61849577+m-gorski@users.noreply.github.com>
2020-05-04 14:49:28 +0200

    Add files via upload

---
+++ Aplikacja webowa/order.php
@@ -0,0 +1,126 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MENU</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<link
+        href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:300,700|Saira:300,400&display=swap&subset=latin-ext"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap&subset=latin-ext" rel="stylesheet">
+		 <!--	do używania ikon z FontAwesome   -->
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="styleOrder.css">
+<style>
+
+p{
+	margin-top: 50px;
+	margin-left: 50px; 
+	width: 50%; 
+	color: black; 
+	font-size: 90px; 
+	font-family: 'Saira Extra Condensed', sans-serif;
+}
+
+
+
+button{
+	 display: block;
+	 height: 100px; 
+	 width: 600px; 
+	 font-size: 40px; 
+	 background: #FACE58; 
+	 margin-left: auto; 
+	 margin-right: auto; 
+	 border-radius: 150px; 
+	 border: none;
+	 padding: 30px;
+}
+
+.one{
+	margin-bottom: 60px;
+}
+
+a.backToHome{
+	color: white; 
+	height: 160px; 
+	width: 160px;  
+	border: none; 
+	font-size: 40px; 
+	line-height: 150px;
+	outline: none; 
+	background-color: #F5585C; 
+	position: fixed; 
+	bottom: 20px; 
+	right: 20px; 
+	text-align: center; 
+	padding: 8px 3px; 
+	text-decoration: none; 
+	border-radius: 10px; 
+}
+
+@media(orientation: landscape) and (min-width: 533px) {
+	p{
+		font-size: 40px;
+	}
+
+	button{
+	 display: block;
+	 height: 80px; 
+	 width: 350px; 
+	 font-size: 22px; 
+	 background: #FACE58; 
+	 margin-left: auto; 
+	 margin-right: auto; 
+	 border-radius: 50px; 
+	 border: none;
+	 padding: 30px;
+}
+
+.one{
+	margin-top: 100px;
+}
+
+a.backToHome{
+	height: 60px; 
+	width: 60px;  
+	border: none; 
+	font-size: 20px; 
+	line-height: 40px;
+}
+
+}
+
+
+</style>
+
+
+	</head>
+	<body>
+
+<div style="display: flex;">
+
+<p class="title" style=" ">Mniam</p>
+
+
+<section style=" width: 50%;">
+
+		</section>
+		</div>
+
+		<div style="width: 100vw; height: 300px; text-align: center; " class="kuchnie">
+		<button class="one" ><a style="text-decoration: none;" href="cusisineTurkish.php">Efes - kuchnia turecka</a></button>
+		<button class="two" ><a style="text-decoration: none;" href="cusisineChinese.php">HoangBao - kuchnia chińska</a></button>
+		</div>
+
+            <!-- przycisk - powrót do strony głównej -->
+		 <!-- <a style=" " class="backToHome" href="index.php">Home</a>  -->
+		<br />
+	
+	
+	</body>
+</html>
+
