698e07384335979826a4e2be7d455bdea81dbb2b
Bartosz Kaczorowski <61693294+BartoszKaczorowski@users.noreply.github.com>
2020-05-28 15:45:23 +0200

    Update food.php

--- Aplikacja mobilna/food.php
+++ Aplikacja mobilna/food.php
@@ -5,7 +5,7 @@ $pass_db = "Helticadb1";
 $db_name = "heltica";
 
 $connection=mysqli_connect('heltica.cba.pl','helticadb','Helticadb1','heltica');
-
+$connection->set_charset("utf8");
 $idLokalu = $_POST['idLokalu'];
 $sql = "SELECT * FROM m_dania WHERE id_lokalu = $idLokalu";
 
