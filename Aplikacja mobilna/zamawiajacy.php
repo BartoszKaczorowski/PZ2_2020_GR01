b3bede18d0416db4b0342931bea52b22d0cfae38
Bartosz Kaczorowski <61693294+BartoszKaczorowski@users.noreply.github.com>
2020-05-28 15:49:08 +0200

    Update zamawiajacy.php

--- Aplikacja mobilna/zamawiajacy.php
+++ Aplikacja mobilna/zamawiajacy.php
@@ -8,7 +8,7 @@ $connection=mysqli_connect('heltica.cba.pl','helticadb','Helticadb1','heltica');
 
 $idUser = $_POST['idUser'];
 $sql = "SELECT * FROM m_zamawiajacy WHERE id_uzytkownika = $idUser";
-
+$connection->set_charset("utf8");
 $result = mysqli_query($connection, $sql);
 	if (mysqli_num_rows($result) > 0){
         $row = mysqli_fetch_array($result);
